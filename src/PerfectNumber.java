ab81a24fad6e2dceb88bfd01255dbf6a8655dab2
Ajay <ajayshaz35@gmail.com>
2023-08-24 15:04:15 +0530

    2nd

---
+++ src/PerfectNumber.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class PerfectNumber {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int num = scan.nextInt();
+
+        int perfect = 0;
+        for(int i=1;i<num;i++){
+            if(num % i ==0){
+                perfect = perfect + i;
+            }
+        }
+        if(perfect==num){
+            System.out.println(num+" is a perfect number.");
+        }else{
+            System.out.println(num+" is not a perfect number.");
+        }
+    }
+}
